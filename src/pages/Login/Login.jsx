37f40c9a97d79ccdce995e085a61618d8117cf86
salvadorperezm <salvadorperezm115@gmail.com>
2023-01-24 16:50:08 -0600

    basic route protecting method added

---
+++ src/pages/Login/Login.jsx
@@ -0,0 +1,9 @@
+import { Box, Input, Text } from "@chakra-ui/react";
+
+export const Login = () => {
+  return (
+    <Box>
+      <Text>Login page</Text>
+    </Box>
+  );
+};
---
+++ src/pages/Register/Register.jsx
@@ -0,0 +1,9 @@
+import { Box, Text } from "@chakra-ui/react";
+
+export const Register = () => {
+  return (
+    <Box>
+      <Text>Register page</Text>
+    </Box>
+  );
+};
