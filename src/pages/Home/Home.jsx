3758c6a2f79a74a026cbbe3cdc421d8832f7c8a0
salvadorperezm <salvadorperezm115@gmail.com>
2023-01-31 15:02:59 -0600

    basic design for the homepage done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,83 @@
-import { Box, Text } from "@chakra-ui/react";
+import { Box, Button, Input, Text } from "@chakra-ui/react";
 import { HomeLayout } from "../../layout";
-import { container } from "./HomeStyles";
+import {
+  container,
+  contentContainer,
+  formContainer,
+  headerText,
+  headerTitle,
+  tasksContainer,
+} from "./HomeStyles";
 
 export const Home = () => {
+  const tasks = [
+    {
+      id: 1,
+      title: "task 1",
+    },
+    {
+      id: 2,
+      title: "task 1",
+    },
+    {
+      id: 3,
+      title: "task 1",
+    },
+    {
+      id: 4,
+      title: "task 1",
+    },
+    {
+      id: 5,
+      title: "task 1",
+    },
+    {
+      id: 6,
+      title: "task 1",
+    },
+    {
+      id: 7,
+      title: "task 1",
+    },
+    {
+      id: 8,
+      title: "task 1",
+    },
+    {
+      id: 9,
+      title: "task 1",
+    },
+    {
+      id: 10,
+      title: "task 1",
+    },
+    {
+      id: 11,
+      title: "task 1",
+    },
+    {
+      id: 12,
+      title: "task 1",
+    },
+  ];
+
   return (
     <HomeLayout>
       <Box {...container}>
-        <Text>Home page</Text>
+        <Box {...contentContainer}>
+          <Box {...headerTitle}>
+            <Text {...headerText}>To-Do List</Text>
+          </Box>
+          <Box {...formContainer}>
+            <Input placeholder="Add a new task" />
+            <Button>Add</Button>
+          </Box>
+          <Box {...tasksContainer}>
+            {tasks.map((task, index) => (
+              <Text key={index}>task.title</Text>
+            ))}
+          </Box>
+        </Box>
       </Box>
     </HomeLayout>
   );
--- src/pages/Home/HomeStyles.js
+++ src/pages/Home/HomeStyles.js
@@ -2,4 +2,43 @@ export const container = {
   minHeight: `100%`,
   background: `rgb(2,0,36)`,
   background: `linear-gradient(90deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%)`,
+  display: `flex`,
+  alignItems: `center`,
+  justifyContent: `center`,
+};
+
+export const contentContainer = {
+  flex: `1`,
+  backgroundColor: `white`,
+  px: { base: `10px`, md: `20px` },
+  py: `20px`,
+  boxShadow: `rgba(99, 99, 99, 0.2) 0px 2px 8px 0px`,
+  borderRadius: { base: `5px`, md: `5px` },
+  mx: { base: `10px`, md: `0px` },
+  maxWidth: `500px`,
+};
+
+export const headerTitle = {
+  borderBottom: `1px solid grey`,
+  pb: `20px`,
+  mb: `20px`,
+};
+
+export const headerText = {
+  fontWeight: `bold`,
+  fontSize: `2rem`,
+  textAlign: `center`,
+};
+
+export const formContainer = {
+  display: `flex`,
+  alignItems: `center`,
+  gap: `10px`,
+  mb: `20px`,
+};
+
+export const tasksContainer = {
+  height: `250px`,
+  border: `1px solid red  `,
+  overflowX: `scroll`,
 };
