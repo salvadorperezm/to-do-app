02577f1804e80afe81875370e6ef9001970cc218
salvadorperezm <salvadorperezm115@gmail.com>
2023-02-01 17:35:37 -0600

    updating header

---
+++ src/components/Menu/HeaderMenu/HeaderMenuStyles.js
@@ -0,0 +1,11 @@
+export const settingsIcon = {
+  color: `white`,
+  size: `lg`,
+  _hover: {
+    backgroundColor: `none`,
+  },
+  _active: {
+    backgroundColor: `white`,
+    color: `black`,
+  },
+};
--- src/components/Menu/HeaderMenu/index.js
+++ src/components/Menu/HeaderMenu/index.js
@@ -1,2 +1,1 @@
-export * from "./TaskMenu";
 export * from "./HeaderMenu";
